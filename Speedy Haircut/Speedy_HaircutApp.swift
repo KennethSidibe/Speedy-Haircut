8208cc29659c84c27da86dd68c20b50d581ad714
Sidibe <Elstevehcks@gmail.com>
2022-06-27 02:12:59 -0400

    Initial Commit

---
+++ Speedy Haircut/Speedy_HaircutApp.swift
@@ -0,0 +1,17 @@
+//
+//  Speedy_HaircutApp.swift
+//  Speedy Haircut
+//
+//  Created by Kenneth Sidibe on 2022-06-27.
+//
+
+import SwiftUI
+
+@main
+struct Speedy_HaircutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
