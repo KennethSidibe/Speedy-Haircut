a96a7ba99ec6a5f113da07f4752dadd4b128dd99
Sidibe <Elstevehcks@gmail.com>
2022-06-28 00:37:27 -0400

    Implement login feature

---
+++ Speedy Haircut/View/SignInView.swift
@@ -0,0 +1,92 @@
+//
+//  ContentView.swift
+//  Speedy Haircut
+//
+//  Created by Kenneth Sidibe on 2022-06-27.
+//
+
+import SwiftUI
+import ChameleonFramework
+import FirebaseAuth
+
+struct SignInView: View {
+    
+    @State private var username: String = ""
+    @State private var password: String = ""
+    @EnvironmentObject var authBrain:AuthenticationBrain
+    
+    var body: some View {
+        
+        
+        ZStack {
+            Color.init(FlatWhite()).ignoresSafeArea()
+            
+                .navigationTitle("Login")
+                .navigationBarTitleDisplayMode(.automatic)
+            
+            VStack(alignment: .center) {
+                
+                Text("Get your haircut FAST")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .padding()
+                    .multilineTextAlignment(.center)
+                
+                Spacer()
+                
+                VStack(alignment: .center) {
+                    
+                    Text("Login").font(.title).bold()
+                    
+                    TextField("Username",text: $username)
+                        .padding()
+                        .frame(width: 300, height: 50, alignment: .center)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                        .disableAutocorrection(true)
+                        .autocapitalization(.none)
+        
+                    
+                    SecureField("password",text: $password)
+                        .padding()
+                        .frame(width: 300, height: 50, alignment: .center)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                    
+                }
+                
+                Button(action: {
+                    
+                    authBrain.signIn(username: username, password: password)
+                    
+                }, label: {
+                    Text("Sign in")
+                        .frame(width: 200, height: 50, alignment: .center)
+                        .foregroundColor(Color.white)
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                        .padding()
+                    
+                })
+                
+                NavigationLink("Create Account", destination: SignUpView())
+                    .foregroundColor(Color.blue)
+                    .padding()
+                
+                Spacer()
+                
+                
+                
+            }
+        }
+    }
+}
+
+struct SignInView_Previews: PreviewProvider {
+    
+    static var previews: some View {
+
+        SignInView()
+            .environmentObject(AuthenticationBrain())
+    }
+}
