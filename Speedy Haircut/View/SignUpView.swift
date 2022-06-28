7ec9351bef748dc94dd179c71a7c72ec3a4ce77f
Sidibe <Elstevehcks@gmail.com>
2022-06-28 00:37:52 -0400

    Implement signup feature

---
+++ Speedy Haircut/View/SignUpView.swift
@@ -0,0 +1,90 @@
+//
+//  SignUpView.swift
+//  Speedy Haircut
+//
+//  Created by Kenneth Sidibe on 2022-06-27.
+//
+
+import SwiftUI
+import ChameleonFramework
+import FirebaseAuth
+
+struct SignUpView: View {
+    
+    @State private var username: String = ""
+    @State private var password: String = ""
+    @EnvironmentObject var authBrain:AuthenticationBrain
+    
+    var body: some View {
+        
+        let backgroundColor = "74b9ff"
+        let textFieldBgImage = "TextField"
+        
+        
+        ZStack {
+            Color.init(FlatWhite()).ignoresSafeArea()
+            
+                .navigationTitle("Sign Up")
+                .navigationBarTitleDisplayMode(.automatic)
+            
+            VStack(alignment: .center) {
+                
+                Text("Get your haircut FAST")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .padding()
+                    .multilineTextAlignment(.center)
+                
+                Spacer()
+                
+                VStack(alignment: .center) {
+                    
+                    Text("Sign up").font(.title).bold()
+                    
+                    TextField("Username",text: $username)
+                        .padding()
+                        .frame(width: 300, height: 50, alignment: .center)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                        .disableAutocorrection(true)
+                        .autocapitalization(.none)
+                    
+                    SecureField("password",text: $password)
+                        .padding()
+                        .frame(width: 300, height: 50, alignment: .center)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                    
+                }
+                
+                Button(action: {
+                    
+                    authBrain.signUp(username: username, password: password)
+                    
+                }, label: {
+                    Text("Sign Up")
+                        .frame(width: 200, height: 50, alignment: .center)
+                        .foregroundColor(Color.white)
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                        .padding()
+                    
+                })
+                
+                Spacer()
+                
+                
+                
+            }
+        }
+    }
+}
+
+struct SignUpView_Previews: PreviewProvider {
+    
+    static var previews: some View {
+
+        SignUpView()
+            .environmentObject(AuthenticationBrain())
+    }
+}
