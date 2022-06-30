3aab5f21b0aad8136f4eababa9ebc5ba8b749ff8
Sidibe <Elstevehcks@gmail.com>
2022-06-30 03:07:52 -0400

    Add hardcoded users collection string

---
+++ Speedy Haircut/Model/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Speedy Haircut
+//
+//  Created by Kenneth Sidibe on 2022-06-29.
+//
+
+import Foundation
+
+struct K {
+    static let userCollectionName = "users"
+    
+}
