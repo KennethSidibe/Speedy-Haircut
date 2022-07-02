cba1926a96e7d34cbe996de0dd7e415e19592d8e
Sidibe <Elstevehcks@gmail.com>
2022-07-02 19:41:51 -0400

    Create a view displaying the current queue

---
+++ Speedy Haircut/View/QueueView.swift
@@ -0,0 +1,24 @@
+//
+//  QueueView.swift
+//  Speedy Haircut
+//
+//  Created by Kenneth Sidibe on 2022-07-02.
+//
+/*
+ One main view of the app that will show a list of all the people that are currently in the queue
+ */
+
+
+import SwiftUI
+
+struct QueueView: View {
+    var body: some View {
+        Text("Here is a list of all the person in the queue")
+    }
+}
+
+struct QueueView_Previews: PreviewProvider {
+    static var previews: some View {
+        QueueView()
+    }
+}
