1499e51a43bd0547f1015139fbc80f97c0ffd1c0
Sidibe <Elstevehcks@gmail.com>
2022-07-02 19:41:23 -0400

    Create view to let the user reserve

---
+++ Speedy Haircut/View/ReservationView.swift
@@ -0,0 +1,20 @@
+//
+//  ReservationView.swift
+//  Speedy Haircut
+//
+//  Created by Kenneth Sidibe on 2022-07-02.
+//
+
+import SwiftUI
+
+struct ReservationView: View {
+    var body: some View {
+        Text("Maker your reservation!")
+    }
+}
+
+struct ReservationView_Previews: PreviewProvider {
+    static var previews: some View {
+        ReservationView()
+    }
+}
