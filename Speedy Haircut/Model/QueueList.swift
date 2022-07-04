a9d521778c6178bd48517f9648777f71339efbe4
Sidibe <Elstevehcks@gmail.com>
2022-07-03 23:55:11 -0400

    Create QueueList Model

---
+++ Speedy Haircut/Model/QueueList.swift
@@ -0,0 +1,13 @@
+//
+//  QueueList.swift
+//  Speedy Haircut
+//
+//  Created by Kenneth Sidibe on 2022-07-02.
+//
+
+import Foundation
+
+struct QueueUser {
+    let name: String
+    let lineNumber:Int
+}
