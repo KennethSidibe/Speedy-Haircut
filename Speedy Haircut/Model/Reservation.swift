8d085c13ad73bc8fc5696f4e29cbc269e4d2f12e
Sidibe <Elstevehcks@gmail.com>
2022-07-06 13:27:10 -0400

    Create reservation model

---
+++ Speedy Haircut/Model/Reservation.swift
@@ -0,0 +1,16 @@
+//
+//  Reservation.swift
+//  Speedy Haircut
+//
+//  Created by Kenneth Sidibe on 2022-07-06.
+//
+
+import Foundation
+
+struct Reservation:Identifiable {
+    
+    let id:String?
+    let clientName:String?
+    let date:Date?
+    
+}
