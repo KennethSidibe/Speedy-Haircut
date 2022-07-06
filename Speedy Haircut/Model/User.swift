d7e0b8697b25553b7785595e08c405183b0834bf
Sidibe <Elstevehcks@gmail.com>
2022-07-06 00:41:21 -0400

    Add Comparable delegate methods

--- Speedy Haircut/Model/User.swift
+++ Speedy Haircut/Model/User.swift
@@ -9,10 +9,52 @@ import Foundation
 
 
 
-class User:Identifiable, ObservableObject {
+class User:Identifiable, ObservableObject, Comparable {
+    
+    
     var id:String?
     var firstName:String?
     var lastName:String?
     var photo:String?
     var lineNumber:Int?
+    
+    //MARK: - Comparable delegate methods
+    
+    static func < (lhs:User, rhs:User) -> Bool {
+        
+        guard lhs.lineNumber != nil, rhs.lineNumber != nil else {
+            
+            print("Line number is nil")
+            return false
+            
+        }
+        
+        return lhs.lineNumber! < rhs.lineNumber!
+        
+    }
+    
+    static func == (lhs: User, rhs: User) -> Bool {
+        
+        guard lhs.lineNumber != nil, rhs.lineNumber != nil else {
+            
+            print("Line number is nil")
+            return false
+        }
+        
+        return lhs.lineNumber! == rhs.lineNumber!
+        
+    }
+    
+    static func > (lhs: User, rhs: User) -> Bool {
+        
+        guard lhs.lineNumber != nil, rhs.lineNumber != nil else {
+            
+            print("Line number is nil")
+            return false
+        }
+        
+        return lhs.lineNumber! > rhs.lineNumber!
+        
+    }
+    
 }
