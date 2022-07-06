3fdb7ca82cd44b783812f32351a72e5ab99b9619
Sidibe <Elstevehcks@gmail.com>
2022-07-06 02:40:26 -0400

    Add queue view feature to show queueList

--- Speedy Haircut/View/QueueView.swift
+++ Speedy Haircut/View/QueueView.swift
@@ -14,10 +14,60 @@ import SwiftUI
 struct QueueView: View {
     
     @EnvironmentObject var dbBrain:DatabaseBrain
+    @State var userList:[User] = [User]()
     
     var body: some View {
         
-        Text("Here is a list of all the person in the queue")
+        VStack {
+            
+            Text("Here is a list of all the person in the queue")
+            
+            List() {
+                
+                ForEach(userList) { user in
+                    
+                    HStack {
+                        
+                        Text(user.firstName!)
+                        
+                        Text(String(user.lineNumber!))
+                        
+                    }
+                }
+                
+                
+            }
+            
+            Button(action: {
+                
+                dbBrain.fetchQueueList { userFetchList in
+                    
+                    DispatchQueue.main.async {
+                        userList = userFetchList
+                    }
+                    
+                }
+                
+            }, label: {
+                Text("Check-in")
+                    .padding()
+                    .frame(width: 150, height: 50, alignment: .center)
+                    .background(Color.black)
+                    .cornerRadius(10)
+                    .foregroundColor(Color.white)
+                
+            })
+            
+        }.onAppear {
+            
+            dbBrain.fetchQueueList { userFetchList in
+                
+                DispatchQueue.main.async {
+                    userList = userFetchList
+                }
+                
+            }
+        }
         
     }
 }
