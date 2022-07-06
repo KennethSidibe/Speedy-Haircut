c7ec83875c2104e22581b76c89e256a1c93828e3
Sidibe <Elstevehcks@gmail.com>
2022-07-06 13:27:19 -0400

    Update reservation view

--- Speedy Haircut/View/ReservationView.swift
+++ Speedy Haircut/View/ReservationView.swift
@@ -8,13 +8,33 @@
 import SwiftUI
 
 struct ReservationView: View {
+    
+    @EnvironmentObject var dbBrain:DatabaseBrain
+    
     var body: some View {
-        Text("Maker your reservation!")
+        Text("Make your reservation!")
+        
+        Button(action: {
+            
+            
+            
+        }, label: {
+            Text("Create reservation now!")
+                .padding()
+                .frame(width: 150, height: 50, alignment: .center)
+                .background(Color.black)
+                .cornerRadius(10)
+                .foregroundColor(Color.white)
+            
+        })
     }
 }
 
 struct ReservationView_Previews: PreviewProvider {
     static var previews: some View {
         ReservationView()
+            .environmentObject(DatabaseBrain())
     }
+    
+    
 }
