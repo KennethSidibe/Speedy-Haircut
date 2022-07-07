784cc4c59d95c82354c21952662ab3108fe6193c
Sidibe <Elstevehcks@gmail.com>
2022-07-07 14:56:14 -0400

    Add Comparable and Equatable delegate methods

--- Speedy Haircut/Model/Reservation.swift
+++ Speedy Haircut/Model/Reservation.swift
@@ -7,10 +7,49 @@
 
 import Foundation
 
-class Reservation:Identifiable, ObservableObject {
+class Reservation:Identifiable, ObservableObject, Comparable, Equatable {
     
     var id:String?
     var clientName:String?
     var date:Date?
+    
+    //MARK: - Comparable delegate methods
+    
+    static func < (lhs:Reservation, rhs:Reservation) -> Bool {
+        
+        guard lhs.date != nil, rhs.date != nil else {
+            
+            print("Line number is nil")
+            return false
+            
+        }
+        
+        return lhs.date! < rhs.date!
+        
+    }
+    
+    static func == (lhs: Reservation, rhs: Reservation) -> Bool {
+        
+        guard lhs.date != nil, rhs.date != nil else {
+            
+            print("Line number is nil")
+            return false
+        }
+        
+        return lhs.date! == rhs.date!
+        
+    }
+    
+    static func > (lhs: Reservation, rhs: Reservation) -> Bool {
+        
+        guard lhs.date != nil, rhs.date != nil else {
+            
+            print("Line number is nil")
+            return false
+        }
+        
+        return lhs.date! > rhs.date!
+        
+    }
     
 }
