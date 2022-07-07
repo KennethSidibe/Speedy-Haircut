c6a0a8ad82f65286bb92db13ab44a5d8b854fce1
Sidibe <Elstevehcks@gmail.com>
2022-07-06 21:07:48 -0400

    Add a date picker who allow reservation until the next year and 2 hours after current time

--- Speedy Haircut/View/ReservationView.swift
+++ Speedy Haircut/View/ReservationView.swift
@@ -10,29 +10,59 @@ import SwiftUI
 struct ReservationView: View {
     
     @EnvironmentObject var dbBrain:DatabaseBrain
+    @State var reservationDate =  Date()
+    @State var range:ClosedRange<Date>?
+    
     
     var body: some View {
-        Text("Make your reservation!")
         
-        Button(action: {
+        VStack() {
             
-            
-            
-        }, label: {
-            Text("Create reservation now!")
+            Text("Make your reservation!")
                 .padding()
-                .frame(width: 150, height: 50, alignment: .center)
-                .background(Color.black)
-                .cornerRadius(10)
-                .foregroundColor(Color.white)
             
-        })
+            DatePicker(
+                "Pick a date",
+                selection: $reservationDate,
+                in:range ?? Date()...Date().addingTimeInterval(86400*365),
+                displayedComponents: [.date, .hourAndMinute])
+            .datePickerStyle(.graphical)
+            .frame(width: 300, alignment: .center)
+            
+            Button(action: {
+                
+                
+                
+            }, label: {
+                Text("Create reservation")
+                    .padding()
+                    .frame(width: 200, height: 50, alignment: .center)
+                    .background(Color.black)
+                    .cornerRadius(10)
+                    .foregroundColor(Color.white)
+                
+            })
+        }
+        .onAppear {
+            
+            let currentYear = Date()
+            let currentDay = Date()
+            let nextYear = Calendar.current.date(byAdding: .year, value: 1, to: currentYear) ?? currentYear.addingTimeInterval(86400*365)
+            
+            let twoHoursBefore = Calendar.current.date(byAdding: .hour, value: -2, to: currentDay) ?? currentDay.addingTimeInterval(-86400*365)
+            
+            range = twoHoursBefore...nextYear
+        }
     }
 }
 
 struct ReservationView_Previews: PreviewProvider {
     static var previews: some View {
-        ReservationView()
+        
+        let rightNow = Date()
+        let nextYear = Calendar.current.date(byAdding: .year, value: 1, to: rightNow)
+        
+        ReservationView(range: Date()...nextYear!)
             .environmentObject(DatabaseBrain())
     }
     
