baf9e30ad3828b47f138b8a02e59debd805ef80b
Sidibe <Elstevehcks@gmail.com>
2022-07-07 21:01:50 -0400

    Create datamodel to store available time for reservation

---
+++ Speedy Haircut/Model/TimeSlot.swift
@@ -0,0 +1,15 @@
+//
+//  TimeSlot.swift
+//  Speedy Haircut
+//
+//  Created by Kenneth Sidibe on 2022-07-07.
+//
+
+import Foundation
+
+class TimeSlot:ObservableObject {
+    
+    var hour:String?
+    var minutesSlot:[String]?
+    
+}
