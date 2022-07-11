f84dcc9a4a9c588689a745b81a1fe4b37edaa53b
Sidibe <Elstevehcks@gmail.com>
2022-07-11 16:38:32 -0400

    Add Comparable Delegate methods

--- Speedy Haircut/Model/DataModel/QueueList.swift
+++ Speedy Haircut/Model/DataModel/QueueList.swift
@@ -7,7 +7,49 @@
 
 import Foundation
 
-struct QueueUser {
-    let name: String
-    let lineNumber:Int
+struct QueueUser:Comparable, Identifiable {
+    let id:String?
+    let name: String?
+    let lineNumber:Int?
+    let timeEnteredQueue:Date?
+    
+    //MARK: - Comparable delegate methods
+    
+    static func < (lhs:QueueUser, rhs:QueueUser) -> Bool {
+        
+        guard lhs.timeEnteredQueue != nil, rhs.timeEnteredQueue != nil else {
+            
+            print("Line number is nil")
+            return false
+            
+        }
+        
+        return lhs.timeEnteredQueue! < rhs.timeEnteredQueue!
+        
+    }
+    
+    static func == (lhs: QueueUser, rhs: QueueUser) -> Bool {
+        
+        guard lhs.timeEnteredQueue != nil, rhs.timeEnteredQueue != nil else {
+            
+            print("Line number is nil")
+            return false
+        }
+        
+        return lhs.timeEnteredQueue! == rhs.timeEnteredQueue!
+        
+    }
+    
+    static func > (lhs: QueueUser, rhs: QueueUser) -> Bool {
+        
+        guard lhs.timeEnteredQueue != nil, rhs.timeEnteredQueue != nil else {
+            
+            print("Line number is nil")
+            return false
+        }
+        
+        return lhs.timeEnteredQueue! > rhs.timeEnteredQueue!
+        
+    }
+    
 }
