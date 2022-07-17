0ee8547028712586d484f561c1d94255e200c99d
Sidibe <Elstevehcks@gmail.com>
2022-07-17 03:43:29 -0400

    Created data model to hold the reservable time slot that the user can choose

---
+++ Speedy Haircut/Model/DataModel/reservableTimeSlot.swift
@@ -0,0 +1,15 @@
+//
+//  reservableTimeSlot.swift
+//  Speedy Haircut
+//
+//  Created by Kenneth Sidibe on 2022-07-17.
+//
+
+import Foundation
+
+struct ReservableTimeSlot {
+    
+    let hour:Int
+    let minutes:[Int]
+    
+}
