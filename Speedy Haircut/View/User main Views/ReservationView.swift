0e430ef408f1b3d61ca4d4e071ba9b6bfc84a4d4
Sidibe <Elstevehcks@gmail.com>
2022-07-23 05:27:17 +0000

    Implement TimePicker code

--- Speedy Haircut/View/User main Views/ReservationView.swift
+++ Speedy Haircut/View/User main Views/ReservationView.swift
@@ -7,6 +7,12 @@
 
 import SwiftUI
 
+struct TimePickerFlipBrains:Identifiable {
+    let id = UUID()
+    let hoursFlipBrains:FlipViewModel
+    let minutesFlipBrains:FlipViewModel
+}
+
 struct ReservationView: View {
     
     @EnvironmentObject var dbBrain:DatabaseBrain
@@ -16,7 +22,10 @@ struct ReservationView: View {
     @State var pickedDate:Date?
     @State var pickedDateString:String?
     @State var pickedTime:Date?
-    var reservationsDate:[Date] {
+    @State var pickedTimeString:String?
+    @State var timePickerFlipBrains:TimePickerFlipBrains?
+    
+    private var reservationsDate:[Date] {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "dd-MM-yyyy HH:mm"
         var reserv = [Date]()
@@ -24,15 +33,37 @@ struct ReservationView: View {
         let reserv2 = dateFormatter.date(from: "23-07-2022 12:35")!
         let reserv3 = dateFormatter.date(from: "23-07-2022 12:50")!
         let reserv4 = dateFormatter.date(from: "25-07-2022 11:20")!
+        let reserv5 = dateFormatter.date(from: "25-07-2022 11:40")!
+        let reserv6 = dateFormatter.date(from: "25-07-2022 11:10")!
+        let reserv7 = dateFormatter.date(from: "25-07-2022 11:14")!
+        let reserv8 = dateFormatter.date(from: "25-07-2022 11:28")!
+        let reserv9 = dateFormatter.date(from: "25-07-2022 11:28")!
+        let reserv10 = dateFormatter.date(from: "25-07-2022 11:28")!
+        let reserv11 = dateFormatter.date(from: "25-07-2022 11:08")!
+        let reserv12 = dateFormatter.date(from: "25-07-2022 11:11")!
+        let reserv13 = dateFormatter.date(from: "25-07-2022 11:44")!
+        let reserv14 = dateFormatter.date(from: "25-07-2022 11:54")!
+        let reserv15 = dateFormatter.date(from: "25-07-2022 11:09")!
         
         reserv.append(reserv1)
         reserv.append(reserv2)
         reserv.append(reserv3)
         reserv.append(reserv4)
+        reserv.append(reserv5)
+        reserv.append(reserv6)
+        reserv.append(reserv7)
+        reserv.append(reserv8)
+        reserv.append(reserv9)
+        reserv.append(reserv10)
+        reserv.append(reserv11)
+        reserv.append(reserv12)
+        reserv.append(reserv13)
+        reserv.append(reserv14)
+        reserv.append(reserv15)
         
         return reserv
     }
-    var queueDates:[Date] {
+    private var queueDates:[Date] {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "dd-MM-yyyy HH:mm"
         var dates = [Date]()
@@ -46,13 +77,15 @@ struct ReservationView: View {
         
         return dates
     }
-    private var dateFormatter = DateFormatter()
-    @State var availableTimeSlot: [Int: [Int]]?
-    @State var minutesFlipBrain:FlipViewModel?
-    @State var hoursFlipBrain:FlipViewModel?
     private var today:String {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "dd-MM-yyyy"
+        
+        return dateFormatter.string(from: Date())
+    }
+    private var todayTime:String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "HH:mm"
         
         return dateFormatter.string(from: Date())
     }
@@ -120,10 +153,25 @@ struct ReservationView: View {
                         .font(.footnote)
                     
                     HStack {
-                        Text("\(reservBrain.getPickedTimeString())")
+                        Text("\(pickedTimeString ?? todayTime)")
                             .frame(width: 200)
                         
                         Button(action: {
+                            
+                            
+                            let hoursFlipModel:FlipViewModel = {
+                                let hoursSlot = reservBrain.getHoursSlotFlipBrain()
+                                return FlipViewModel(timeSlot: hoursSlot)
+                            }()
+                                
+                            let minutesFlipModel:FlipViewModel = {
+                                let firstMinutesSlot:[String] = reservBrain.getFirstMinutesSlot()
+                                return FlipViewModel(timeSlot: firstMinutesSlot)
+                            }()
+                            
+                            self.timePickerFlipBrains = TimePickerFlipBrains(
+                                hoursFlipBrains: hoursFlipModel,
+                                minutesFlipBrains: minutesFlipModel)
                             
                             isTimePickerShow = true
                             
@@ -143,39 +191,19 @@ struct ReservationView: View {
                 }
                 
             }
-            .sheet(isPresented: $isTimePickerShow) {
-                
-                let reservableTimeSlot = dbBrain.createReservationTimeSlot(lastTimeToReserve: nil, dateToReserve: pickedDate ?? Date())
-                
-                let optionalAvailableTimeSlot = Dictionary(
-                    uniqueKeysWithValues: reservableTimeSlot.map(
-                        { timeSlot in
-                            (timeSlot.hour, timeSlot.minutes)
-                        }
-                    )
-                )
-                
-                let optionalHourSlot = optionalAvailableTimeSlot.keys.sorted()
-                let optionalHourString = optionalHourSlot.map { key in
-                    return String(key)
-                }
-                
-                let optionalMinutesSlot:[Int] = optionalAvailableTimeSlot[optionalHourSlot.first!]!
-                let optionalMinutesSlotString = optionalMinutesSlot.map { value -> String in
-                    if value == 0 {
-                        return "01"
-                    }
-                    return String(value)
-                }
-                
-                
-                let optionalHourFlipBrain = FlipViewModel(timeSlot: optionalHourString)
-                let optionalMinuteFlipBrain = FlipViewModel(timeSlot: optionalMinutesSlotString)
-
-                
-                TimePickerView(hourBrain: self.hoursFlipBrain ?? optionalHourFlipBrain, minuteBrain: self.minutesFlipBrain ?? optionalMinuteFlipBrain, timePicked: $pickedTime, isPresented: $isTimePickerShow, availableTimeSlot: self.availableTimeSlot ?? optionalAvailableTimeSlot)
+            .sheet(item: $timePickerFlipBrains) { flipBrains in
+                TimePickerView(hourBrain: flipBrains.hoursFlipBrains,
+                               minuteBrain: flipBrains.minutesFlipBrains,
+                               timePicked: $pickedTime,
+                               timePickedString: $pickedTimeString,
+                               timePickerFlipBrains: $timePickerFlipBrains,
+                               availableTimeSlot: reservBrain.getAvailableTimeSlot()!)
                 
             }
+            
+            
+            
+            
             
             Text("Time")
                 .font(.title3)
@@ -214,6 +242,15 @@ struct ReservationView: View {
                 
                 let reservations = self.reservationsDate
                 let queueDates = self.queueDates
+                
+                var testDate:Date = {
+                    var dateFormatter = DateFormatter()
+                    dateFormatter.dateFormat = "dd-MM-yyyy"
+                    let date = dateFormatter.date(from: "23-07-2022")!
+                    return date
+                }()
+                
+                self.pickedDate = testDate
                 
                 reservBrain.setBrain(reservations: reservations, queueDates: queueDates, datePicked: pickedDate)
 
