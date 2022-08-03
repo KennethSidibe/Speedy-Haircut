3dcd111e13574bae82d783c9cbe436ee52f9d3fa
Sidibe <Elstevehcks@gmail.com>
2022-08-03 21:48:32 +0000

    remove space from comment

--- Speedy Haircut/View/Sign Views/ProfileSwitchView.swift
+++ Speedy Haircut/View/Sign Views/ProfileSwitchView.swift
@@ -36,7 +36,7 @@ struct ProfileSwitchView: View {
                             self.dbBrain.setUser(user: currentUser)
                             await self.dbBrain.setDatabaseBrain()
                             
-                            //                        Delay to let loading animation play
+                            // Delay to let loading animation play
                             DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(1)) {
                                 
                                     self.dbBrain.setIsDataAvailable(true) 
