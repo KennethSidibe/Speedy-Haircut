a15d4229d6b7255412a75350bd8586196d15e49e
Sidibe <Elstevehcks@gmail.com>
2022-08-04 15:55:35 +0000

    Create Admin object

---
+++ Speedy Haircut/Model/DataModel/Admin.swift
@@ -0,0 +1,15 @@
+//
+//  Admin.swift
+//  Speedy Haircut
+//
+//  Created by Kenneth Sidibe on 2022-08-04.
+//
+
+import Foundation
+
+class Admin:Identifiable, ObservableObject {
+    
+    var name:String?
+    var privilege:String?
+    
+}
